c89619576a6c2c12601594261b09a019525307f1
Ekwere Noble <ekwerenoble2@gmail.com>
2026-09-24 10:05:23 +0100

    a11y: hide decorative category icons

--- fixit-customer/src/home/Category.jsx
+++ fixit-customer/src/home/Category.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 function Category({ icon = 'fa-solid fa-screwdriver-wrench', name = 'General Services', onSelect }) {
     return (
         <button type="button" className="category" onClick={onSelect}>
-            <span className="category-icon" aria-hidden="true"><i className={icon}></i></span>
+            <span className="category-icon" aria-hidden="true"><i aria-hidden="true" className={icon}></i></span>
             <span className="category-name">{name}</span>
         </button>
     );
